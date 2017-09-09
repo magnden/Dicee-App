c6ab6ce290e7996ee77ccc5e5a5a88de7c9c3055
Dennis Magnusson <dennis@Dennis-MacBook-Pro.local>
2017-09-09 19:42:59 +0300

    Initial Commit

---
+++ dicee-app/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  dicee-app
+//
+//  Created by Dennis Magnusson on 09/09/2017.
+//  Copyright © 2017 Dennis Magnusson. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+
+    let diceImages = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+    
+    var rollSound: AVAudioPlayer!
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupSound()
+        updateDiceImages()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+   
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages(){
+        
+        rollSound.play()
+        
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        diceImageView1.image = UIImage(named: diceImages[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceImages[randomDiceIndex2])
+        
+    }
+    
+    func setupSound(){
+        let musicFile = Bundle.main.path(forResource: "roll", ofType: ".wav")!
+        let url = URL(fileURLWithPath: musicFile)
+        do {
+            rollSound = try AVAudioPlayer(contentsOf: url)
+            rollSound.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+}
+
